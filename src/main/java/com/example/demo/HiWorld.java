a91650364dad676427a7653e1b9200c90599c574
QUT201607384 <runxiao1998@163.com>
2021-05-20 16:23:28 +0800

    test

--- src/main/java/com/example/demo/HiWorld.java
+++ src/main/java/com/example/demo/HiWorld.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("Hi World!");
+        System.out.println("Hi World!");
     }
 }
